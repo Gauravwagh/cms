54e19df7800d3649aac181923ffcc875f66957bb
Gauravwagh <gauravwagh11@gmail.com>
2016-05-09 19:51:47 +0530

    Changes in gunicorn 2

--- django15/wsgi.py
+++ django15/wsgi.py
@@ -32,14 +32,6 @@
 # # application = HelloWorldApplication(application)
 
 
-# from django.core.wsgi import get_wsgi_application
-# from dj_static import Cling
-#
-# application = Cling(get_wsgi_application())
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django15.settings")
-
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
